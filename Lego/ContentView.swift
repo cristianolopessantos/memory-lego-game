7ef4b72397f86f30a87f4ab6e85f19107f038d34
cristianolopessantos <cristianolopes@dcc.ufrj.br>
2022-06-16 15:09:19 -0300

    Initial Commit

---
+++ Lego/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lego
+//
+//  Created by Cristiano  Santos on 16/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lego/LegoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegoApp.swift
+//  Lego
+//
+//  Created by Cristiano  Santos on 16/06/22.
+//
+
+import SwiftUI
+
+@main
+struct LegoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
